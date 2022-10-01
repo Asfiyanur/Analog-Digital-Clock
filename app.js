5005f2df6345d086b11cee149bfbf2810536d1af
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-01 19:13:11 +0300

    selectors

---
+++ app.js
@@ -0,0 +1,5 @@
+const hours_needle = document.querySelector(".hours-needle");
+const minutes_needle = document.querySelector(".minutes-needle");
+const seconds_needle = document.querySelector(".seconds-needle");
+
+const diigital = document.querySelector(".digital");
