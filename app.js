53b9c1caff0892e38e07c965f2bdf81b041be792
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-01 19:39:21 +0300

    js-done

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ setInterval(() => {
   seconds_needle.style.transform = `translate(-50%, -100%) rotate(${
     (360 * seconds) / 60
   }deg)`;
+
+  if (hours < 10) hours = `0${hours}`;
+  if (minutes < 10) minutes = `0${minutes}`;
+  if (seconds < 10) seconds = `0${seconds}`;
+
   diigital.innerHTML = ` <span class="hours">${hours}</span>:
                 <span class="minutes">${minutes}</span>:
                 <span class="seconds">${seconds}</span>`;
