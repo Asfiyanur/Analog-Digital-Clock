2cc4aa5ac553cebc47fb00e2a8fee0db2521ea7c
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-01 19:31:37 +0300

    analog-js-done

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const minutes_needle = document.querySelector(".minutes-needle");
 const seconds_needle = document.querySelector(".seconds-needle");
 
 const diigital = document.querySelector(".digital");
+
+setInterval(() => {
+  const time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  hours_needle.style.transform = `translate(-50%, -100%) rotate(${
+    (360 * hours) / 12
+  }deg)`;
+  minutes_needle.style.transform = `translate(-50%, -100%) rotate(${
+    (360 * minutes) / 60
+  }deg)`;
+  seconds_needle.style.transform = `translate(-50%, -100%) rotate(${
+    (360 * seconds) / 60
+  }deg)`;
+}, 1000);
